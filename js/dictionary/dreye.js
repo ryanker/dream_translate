3993f7faa1f9f2810c29caad670badb42d6b179a
Ryan <ryanker1024@gmail.com>
2020-12-05 18:01:35 +0800

    增加译典通

---
+++ js/dictionary/dreye.js
@@ -0,0 +1,36 @@
+'use strict'
+
+function dreyeDictionary() {
+    return {
+        init() {
+            return this
+        },
+        unify(r, q) {
+            let el = r.querySelector('.q_middle')
+
+            // 清理
+            el.querySelectorAll('script,style').forEach(e => {
+                e.remove()
+            })
+            return {text: q, phonetic: {}, sound: [], html: el.innerHTML}
+        },
+        query(q) {
+            return new Promise((resolve, reject) => {
+                if (q.length > 100) return reject('The text is too large!')
+                let url = `https://www.dreye.com.cn/dict_new/dict_min.php?w=${encodeURIComponent(q)}`
+                httpGet(url, 'document').then(r => {
+                    if (r) {
+                        resolve(this.unify(r, q))
+                    } else {
+                        reject('dreye.com.cn error!')
+                    }
+                }).catch(e => {
+                    reject(e)
+                })
+            })
+        },
+        link(q) {
+            return `https://www.dreye.com.cn/dict_new/dict_min.php?w=${encodeURIComponent(q)}`
+        },
+    }
+}
