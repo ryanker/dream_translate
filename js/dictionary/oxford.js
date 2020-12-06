6f5de9644084d3980a046b45d69cb857d5119cad
Ryan <ryanker1024@gmail.com>
2020-12-07 00:40:22 +0800

    增加两款国外词典

---
+++ js/dictionary/oxford.js
@@ -0,0 +1,36 @@
+'use strict'
+
+function oxfordDictionary() {
+    return {
+        init() {
+            return this
+        },
+        unify(r, q) {
+            let el = r.querySelector('#entryContent')
+
+            // 清理
+            el.querySelectorAll('script,style').forEach(e => {
+                e.remove()
+            })
+            return {text: q, phonetic: {}, sound: [], html: el.innerHTML}
+        },
+        query(q) {
+            return new Promise((resolve, reject) => {
+                if (q.length > 100) return reject('The text is too large!')
+                let url = `https://www.oxfordlearnersdictionaries.com/definition/english/${encodeURIComponent(q)}`
+                httpGet(url, 'document').then(r => {
+                    if (r) {
+                        resolve(this.unify(r, q))
+                    } else {
+                        reject('oxfordlearnersdictionaries.com error!')
+                    }
+                }).catch(e => {
+                    reject(e)
+                })
+            })
+        },
+        link(q) {
+            return `https://www.oxfordlearnersdictionaries.com/definition/english/${encodeURIComponent(q)}`
+        },
+    }
+}
