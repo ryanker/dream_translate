7bee50ac57e23587a3ee1b7675874d9f1d3bc245
Ryan <ryanker1024@gmail.com>
2020-12-30 11:40:26 +0800

    改进，某些网站报错问题

--- src/js/frame.js
+++ src/js/frame.js
@@ -7,17 +7,25 @@
  * @license MIT License
  */
 
-let frame = window.frameElement
-if (frame) {
-    let top = window.top
-    document.addEventListener('mouseup', function (e) {
-        let bcr = frame.getBoundingClientRect()
-        let clientX = e.clientX + bcr.left
-        let clientY = e.clientY + bcr.top
-        let text = window.getSelection().toString().trim()
-        if (text) top.postMessage({text: text, clientX: clientX, clientY: clientY}, '*')
-    })
-    document.addEventListener('mouseup', function () {
-        top._MxDialog && top._MxDialog.hide()
-    })
-}
+;(function () {
+    let frame = window.frameElement
+    if (frame) {
+        let top = null
+        try {
+            top = window.top || window.parent
+        } catch (e) {
+            return
+        }
+        if (!top) return
+        document.addEventListener('mouseup', function (e) {
+            let bcr = frame.getBoundingClientRect()
+            let clientX = e.clientX + bcr.left
+            let clientY = e.clientY + bcr.top
+            let text = window.getSelection().toString().trim()
+            if (text) top.postMessage({text: text, clientX: clientX, clientY: clientY}, '*')
+        })
+        document.addEventListener('mouseup', function () {
+            top._MxDialog && top._MxDialog.hide()
+        })
+    }
+})()
