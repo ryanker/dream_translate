89f9eed131eb92580348190995a9b60ac7cf987f
Ryan <ryanker1024@gmail.com>
2021-01-01 22:20:20 +0800

    完善人人词典

--- src/js/dictionary/rrdict.js
+++ src/js/dictionary/rrdict.js
@@ -14,12 +14,58 @@ function rrdictDictionary() {
         },
         unify(r, q) {
             let el = r.querySelector('section.content')
+            let s = ''
 
-            // 清理
-            el.querySelectorAll('script,style').forEach(e => {
-                e.remove()
+            // 查询单词
+            let wordEl = el.querySelector('.text')
+            if (wordEl) s = `<div class="case_dd_head">${wordEl.innerText.trim()}</div>`
+
+            let phonetic = {} // 音标
+            let sound = [] // 发音
+            console.log(el.querySelectorAll('.vos > span > a'))
+            el.querySelectorAll('.vos > span').forEach((e, k) => {
+                let ph = e.innerText && e.innerText.replace(/[\[\]英美]/g, '').trim() || ''
+                if (!ph) return
+                let type = ''
+                if (k === 0) {
+                    phonetic.uk = ph
+                    type = 'uk'
+                } else {
+                    phonetic.us = ph
+                    type = 'us'
+                }
+                let auEl = e.querySelector('audio[src]')
+                if (!auEl) return
+                sound.push({type, url: auEl.src})
             })
-            return {text: q, phonetic: {}, sound: [], html: el.innerHTML}
+            if (phonetic.us && phonetic.uk === phonetic.us) delete phonetic.us // 如果音标一样，只保留一个
+
+            // 释义
+            let liEl = el.querySelector('.tmInfo .listBox')
+            if (liEl && liEl.childNodes.length > 0) {
+                s += `<div class="case_dd_parts">`
+                liEl.childNodes.forEach(node => {
+                    if (node.nodeType === 3) {
+                        let part = node.textContent.replace(/^[a-zA-Z]+\.\s+/, function (str, k) {
+                            return k === 0 ? `<b>${str.trim()}</b>` : str
+                        })
+                        if (part) s += `<p>${part}</p>`
+                    }
+                })
+                s += `</div>`
+            }
+
+            // 单词形态
+            let transEl = el.querySelector('.tmInfo .listBox:nth-child(2)')
+            if (transEl) {
+                let shapeStr = transEl.innerText.trim()
+                shapeStr = shapeStr.replace(/[a-z]+/ig, function (str) {
+                    return `<a data-search="true">${str}</a>`
+                })
+                s += `<div class="case_dd_exchange">${shapeStr}</div>`
+            }
+
+            return {text: q, phonetic, sound, html: s}
         },
         query(q) {
             return new Promise((resolve, reject) => {
