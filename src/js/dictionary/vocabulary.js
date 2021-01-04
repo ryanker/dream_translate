a2398ebdd0285b12dd930c9e302e56cf4a7c013f
Ryan <ryanker1024@gmail.com>
2021-01-04 20:22:00 +0800

    完善词汇词典

--- src/js/dictionary/vocabulary.js
+++ src/js/dictionary/vocabulary.js
@@ -9,23 +9,33 @@
 
 function vocabularyDictionary() {
     return {
+        url: 'https://www.vocabulary.com/dictionary/',
         init() {
             return this
         },
         unify(r, q) {
             let el = r.querySelector('.centeredContent')
 
+            // 发音
+            let sound = []
+            let soundEl = r.querySelector('a.audio[data-audio]')
+            if (soundEl) sound.push({type: 'us', url: `https://audio.vocab.com/1.0/us/${soundEl.dataset.audio}.mp3`})
+
             // 清理
-            el.querySelectorAll('script,style').forEach(e => {
-                e.remove()
+            removeD(el.querySelectorAll('script,style,img'))
+            cleanAttr(el, ['title', 'class'])
+            el.querySelectorAll('.groupNumber').forEach(e => {
+                e.style.marginTop = '10px'
+                e.style.fontWeight = '700'
             })
-            return {text: q, phonetic: {}, sound: [], html: el.innerHTML}
+
+            return {text: q, phonetic: {}, sound, html: el.innerHTML}
         },
         query(q) {
             return new Promise((resolve, reject) => {
                 if (q.length > 100) return reject('The text is too large!')
-                let url = `https://www.vocabulary.com/dictionary/${encodeURIComponent(q)}`
-                httpGet(url, 'document').then(r => {
+                let url = this.url + encodeURIComponent(q)
+                httpGet(url, 'document', null, true).then(r => {
                     if (r) {
                         resolve(this.unify(r, q))
                     } else {
@@ -37,7 +47,7 @@ function vocabularyDictionary() {
             })
         },
         link(q) {
-            return `https://www.vocabulary.com/dictionary/${encodeURIComponent(q)}`
+            return this.url + encodeURIComponent(q)
         },
     }
 }
