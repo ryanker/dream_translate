5d8fb761fedb9a54c39362e72827dc919b355be3
Ryan <ryanker1024@gmail.com>
2021-01-04 21:04:54 +0800

    完善城市词典

--- src/js/dictionary/urban.js
+++ src/js/dictionary/urban.js
@@ -9,23 +9,27 @@
 
 function urbanDictionary() {
     return {
+        url: 'https://www.urbandictionary.com/define.php?term=',
         init() {
             return this
         },
         unify(r, q) {
-            let el = r.querySelector('#content')
+            let el = r.querySelector('#content > .def-panel')
 
-            // 清理
-            el.querySelectorAll('script,style').forEach(e => {
-                e.remove()
+            removeD(el.querySelectorAll('script,style,.row,.def-footer,a.mug-ad'))
+            cleanAttr(el, ['title', 'class', 'autoplay', 'loop', 'muted', 'playsinline', 'src', 'width', 'height'])
+            el.querySelectorAll('.def-header').forEach(e => {
+                e.style.fontSize = '120%'
+                e.style.fontWeight = '700'
             })
+
             return {text: q, phonetic: {}, sound: [], html: el.innerHTML}
         },
         query(q) {
             return new Promise((resolve, reject) => {
                 if (q.length > 100) return reject('The text is too large!')
-                let url = `https://www.urbandictionary.com/define.php?term=${encodeURIComponent(q)}`
-                httpGet(url, 'document').then(r => {
+                let url = this.url + encodeURIComponent(q)
+                httpGet(url, 'document', null, true).then(r => {
                     if (r) {
                         resolve(this.unify(r, q))
                     } else {
@@ -37,7 +41,7 @@ function urbanDictionary() {
             })
         },
         link(q) {
-            return `https://www.urbandictionary.com/define.php?term=${encodeURIComponent(q)}`
+            return this.url + encodeURIComponent(q)
         },
     }
 }
