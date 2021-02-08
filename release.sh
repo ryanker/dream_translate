baba3159bc9d4045453b1385a8ef2f47c06df2dd
Ryan <ryanker1024@gmail.com>
2021-02-08 13:01:50 +0800

    优化

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ mkdir -p $DIST
 \cp -af ./src/ $DIST
 
 # 打包 (Chrome Edge)
-sed -i "" "s/const isDebug = true/const isDebug = false/g" $DIST/js/common.js
+sed -i "" -e "s/const isDebug = true/const isDebug = false/g" $DIST/js/common.js
 zip -rq dist.zip $DIST
 
 # ======== Firefox ========
@@ -20,7 +20,7 @@ mkdir -p $DIST2
 \cp -af ./src/ $DIST2
 
 # 替换
-sed -i "" "s/const isDebug = true/const isDebug = false/g" $DIST2/js/common.js
+sed -i "" -e "s/const isDebug = true/const isDebug = false/g" $DIST2/js/common.js
 sed -i "" -e '16,21d' $DIST2/css/content.css # 清理 CSS 样式
 sed -i "" -e '/"tts",/d' $DIST2/manifest.json
 sed -i "" -e '/"global": true,/d' $DIST2/manifest.json
